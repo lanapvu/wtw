9d44682eb8df809fdf69c13910a218682e49c635
hanabel-m <hmengistu@umass.edu>
2023-11-11 18:27:25 -0500

    Wonky bar

--- wtw/Views/HomeView.swift
+++ wtw/Views/HomeView.swift
@@ -4,15 +4,64 @@
 //
 //  Created by Lord Custodio on 11/11/23.
 //
-
 import SwiftUI
 
-struct HomeView: View {
+struct ContentView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                // Your main content view
+                    
+
+                TabView {
+                    // Home tab
+                   Text("Home")
+                        .tabItem {
+                            Image(systemName: "house.fill")
+                        }
+                        .tag(0)
+                    //Messages tab
+                    Text("Messages")
+                        .tabItem {
+                            Image(systemName: "chat")
+                        }
+                    //Add tab
+                    Text("Add")
+                        .tabItem {
+                            Image(systemName: "plus")
+                            Text("Add")
+                        }
+                    // Settings tab
+                    Text("Settings")
+                        .tabItem {
+                            Image(systemName: "gear")
+                            Text("Settings")
+                        }
+
+                }
+                .accentColor(.green) // Set tab icon color
+                .padding()
+                Color.green
+                    .frame(maxHeight: 40)
+            }
+            }
+            .navigationBarTitle("wtw")
+            
+
+        }
+    }
+
+
+struct ColorView: View {
+    var body: some View {
+        Text("")
+            .frame(maxWidth: .infinity , maxHeight: 40)
+            .background(Color.green)
     }
 }
 
-#Preview {
-    HomeView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
