7d63f7b287f6bea65e2b876d20ae1942f70720e5
Lord Custodio <lcustodio@umass.edu>
2023-11-11 13:59:26 -0500

    Initial Commit

---
+++ wtw/wtwApp.swift
@@ -0,0 +1,17 @@
+//
+//  wtwApp.swift
+//  wtw
+//
+//  Created by Lord Custodio on 11/11/23.
+//
+
+import SwiftUI
+
+@main
+struct wtwApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
