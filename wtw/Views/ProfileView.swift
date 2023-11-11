396611429d70a9d3af4d6f7384aa68a0cd8b6014
Lord Custodio <lcustodio@umass.edu>
2023-11-11 15:25:16 -0500

    Change views

---
+++ wtw/Views/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  wtw
+//
+//  Created by Lord Custodio on 11/11/23.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
