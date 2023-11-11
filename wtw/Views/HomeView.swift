011573d3560fa5e5cb85785576eff34084667c3a
Lord Custodio <lcustodio@umass.edu>
2023-11-11 15:18:10 -0500

    Folder

---
+++ wtw/Views/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  wtw
+//
+//  Created by Lord Custodio on 11/11/23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
