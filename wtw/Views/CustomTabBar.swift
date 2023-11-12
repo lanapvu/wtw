41ee18bf7c272c4d368801733ec138095ff329e1
hanabel-m <hmengistu@umass.edu>
2023-11-11 20:41:11 -0500

    Bottom bar

---
+++ wtw/Views/CustomTabBar.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTabBar.swift
+//  wtw
+//
+//  Created by Hanabel Mengistu on 11/11/23.
+//
+
+import SwiftUI
+
+struct CustomTabBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomTabBar()
+}
