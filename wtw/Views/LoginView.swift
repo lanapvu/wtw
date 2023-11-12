c2c1b54e3364a60856c5c7e40196ef0d9aaec228
hanabel-m <hmengistu@umass.edu>
2023-11-12 01:30:44 -0500

    All of Lanas changes

--- wtw/Views/LoginView.swift
+++ wtw/Views/LoginView.swift
@@ -2,68 +2,14 @@
 //  LoginView.swift
 //  wtw
 //
-//  Created by lordpoo on 11/12/23.
+//  Created by Hanabel Mengistu on 11/12/23.
 //
 
 import SwiftUI
 
 struct LoginView: View {
-    @State private var email: String = ""
-    @State private var password: String = ""
-
     var body: some View {
-        NavigationView {
-            VStack(spacing: 16) {
-                Image(systemName: "person.crop.circle.fill")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 80, height: 80)
-                    .foregroundColor(.green)
-                    .padding(.top, 100)
-                
-                Text("Login")
-                    .font(.largeTitle)
-                    .fontWeight(.semibold)
-                
-                Text("Please sign in to continue")
-                    .font(.body)
-                    .foregroundColor(.gray)
-                
-                TextField("Email", text: $email)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .autocapitalization(.none)
-                    .keyboardType(.emailAddress)
-                
-                SecureField("Password", text: $password)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                
-                Button("Forgot password?") {
-                    // Action to reset password
-                }
-                .foregroundColor(.green)
-                .padding(.trailing, 10)
-                .alignmentGuide(.trailing) { d in d[.trailing] }
-                .frame(maxWidth: .infinity, alignment: .trailing)
-
-                Button("Sign In") {
-                    // Action to perform login
-                }
-                .frame(maxWidth: .infinity)
-                .padding()
-                .background(Color.green)
-                .foregroundColor(.white)
-                .cornerRadius(10)
-
-                Spacer()
-                
-                NavigationLink("Don't have an account? Sign Up", destination: SignUpView())
-                    .foregroundColor(.green)
-                
-                Spacer()
-            }
-            .padding(.horizontal, 32)
-            .navigationBarHidden(true)
-        }
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
--- wtw/Views/SignUpView.swift
+++ wtw/Views/SignUpView.swift
@@ -1,68 +1,18 @@
 //
-//  SignUpView.swift
+//  SignupView.swift
 //  wtw
 //
-//  Created by lordpoo on 11/12/23.
+//  Created by Hanabel Mengistu on 11/12/23.
 //
 
 import SwiftUI
 
-struct SignUpView: View {
-    @State private var name: String = ""
-    @State private var email: String = ""
-    @State private var password: String = ""
-    @State private var confirmPassword: String = ""
-
+struct SignupView: View {
     var body: some View {
-        VStack(spacing: 16) {
-            Image(systemName: "person.badge.plus")
-                .resizable()
-                .scaledToFit()
-                .frame(width: 80, height: 80)
-                .foregroundColor(.green)
-                .padding(.top, 100)
-            
-            Text("Create Account")
-                .font(.largeTitle)
-                .fontWeight(.semibold)
-            
-            TextField("Name", text: $name)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .autocapitalization(.words)
-            
-            TextField("Email", text: $email)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .autocapitalization(.none)
-                .keyboardType(.emailAddress)
-            
-            SecureField("Password", text: $password)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-            
-            SecureField("Confirm Password", text: $confirmPassword)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-            
-            Button("Sign Up") {
-                // Action to perform sign up
-            }
-            .frame(maxWidth: .infinity)
-            .padding()
-            .background(Color.green)
-            .foregroundColor(.white)
-            .cornerRadius(10)
-            
-            Spacer()
-            
-            NavigationLink("Already have an account? Sign In", destination: LoginView())
-                .foregroundColor(.green)
-            
-            Spacer()
-        }
-        .padding(.horizontal, 32)
-        .navigationBarHidden(true)
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
-
 #Preview {
-    SignUpView()
+    SignupView()
 }
